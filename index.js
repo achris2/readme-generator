68fc3b0912e3dad78aafe8246fe639a1013a4135
Alex Christakou <alex@Alexs-Air-2.broadband>
2024-01-16 21:03:34 +0000

    finished generate Markdown JS file

--- index.js
+++ index.js
@@ -9,42 +9,104 @@ const generateMarkdown = require("./utils/generateMarkdown");
 // 3. Generate the README.md content based on the user input 
 // 4. write to README.md file 
 
+// When a user is prompted for information about the application repository then a high-quality, professional README.md is generated with:
+// The title of my project
+// Sections entitled:
+// Description
+// Table of Contents
+// Installation
+// Usage
+// License
+// Contributing
+// Tests
+// Questions
+// When a user enters the project title then it is displayed as the title of the README
+// When a user enters a description, installation instructions, usage information, contribution guidelines, and test instructions then this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// When a user chooses a license for their application from a list of options then a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// When a user enters their GitHub username then this is added to the section of the README entitled Questions, with a link to their GitHub profile
+// When a user enters their email address then this is added to the section of the README entitled Questions, with instructions on how to reach them with additional questions
+// When a user clicks on the links in the Table of Contents then they are taken to the corresponding section of the README
 
 
-// array of questions for user
+
 const questions = [
-    inquirer.prompt([
         {
             type: 'input',
             name: 'title',
-            message: 'What is your project',
-        }
-])
-// then // ... set the answers to constants 
+            message: 'What is your project title?',
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please describe your project',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Please include the steps and installation instructions for your project',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Please provide instructions and examples for use',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Please select a license',
+            choices: ["MIT License","Creative Commons", "GNU General Public License", "Mozilla Public License 2.0", "None"],
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'If applicable, please provide guidelines for contributing to this project ',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Please provide any tests for your application and examples on how to run them.',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your GitHub username?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email?',
+        },
 
-.then((answers)) => {
-    const titleAnswer = answers.title
-    console.log(titleAnswer)
-    // call the write to file function 
-
-}
-
-];
+]
 
 
-// 
-// parseInt()?
 // function to write README file
-function writeToFile(fileName, data) {
-    // pass user answers to the generate markdown function 
-    // generateMarkdown 
-
-    fs.writeFile(fileName, answers, (err)) => 
+function writeToFile(fileName, answers) {
+    return new Promise((resolve,reject)=> {
+    fs.writeFile(fileName, answers, (err) =>{
+        if (err){
+            reject (err);
+        } else {
+            resolve();
+        }
+    });
+    
+}); 
 }
 
 // function to initialize program
 function init() {
+    inquirer.prompt(questions).then((answers) => {
+        const fileName = path.join("./results",`${answers.title}_README.md`);
+        writeToFile(fileName, generateMarkdown(answers))
+        .then (()=> {
+            console.log("Your README.md file has been succesfully generated!"); 
+        })
+        .catch((err) => {
+            console.error(err);
+        });
 
+    })
 }
 
 // function call to initialize program
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,49 @@
+// generate license badge 
+function generateLicense(license) {
+  if (license !== "none"){
+    return `![License](https://img.shields.io/badge/license-${license}-blue.svg)`
+  } else {
+    return ""; 
+  }
+}
+
 // function to generate markdown for README
+// Must include: Table of Contents, Description, Installation, Usage, License, Contributing, Tests, Questions
+
 function generateMarkdown(answers) {
-  return `# ${answers.title}
+  return `
+  # ${answers.title}
+  ${generateLicense(answers.license)}
+
+  # Table of Contents 
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## Description 
+  ${answers.description}
+
+  ##Installation
+  ${answers.installation}
+
+  ## Usage
+  ${answers.description}
+
+  ## License
+  This project is licensed under ${answers.license}
+
+  ## Contributing
+  ${answers.contributing}
+
+  ## Tests
+  ${answers.tests}
+
+  ## Questions
+  If you have any questions, please feel free to reach out on my GitHub [github/${answers.github}](https://github.com/${answers.github}) or email me by [here](mailto:${answers.email}?subject=${answers.title})
 
 `;
 }
