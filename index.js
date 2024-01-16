a2b7fa56ec302d7b25f6ecaa9d7d41b4965c7e3f
Alex Christakou <alex@Alexs-Air-2.broadband>
2024-01-16 16:01:30 +0000

    updated git ignore, started to include questions

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
-const fs = require("fs");
+const fs = require("fs"); // node module that uses file
 const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
+// 1. Come up with Readme Structure 
+// 2. Decide which questions you want to ask and the question format
+// e.g. Licence should be a select box, description may be a text entry box 
+// 3. Generate the README.md content based on the user input 
+// 4. write to README.md file 
+
+
+
 // array of questions for user
 const questions = [
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is your project',
+        }
+])
+// then // ... set the answers to constants 
+
+.then((answers)) => {
+    const titleAnswer = answers.title
+    console.log(titleAnswer)
+    // call the write to file function 
+
+}
 
 ];
 
+
+// 
+// parseInt()?
 // function to write README file
 function writeToFile(fileName, data) {
+    // pass user answers to the generate markdown function 
+    // generateMarkdown 
+
+    fs.writeFile(fileName, answers, (err)) => 
 }
 
 // function to initialize program
